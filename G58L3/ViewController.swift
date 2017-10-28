10e07901a1dc4338b611ee52e51f3ca4a56d345c
Tung Fam <tung@pixpie.co>
2017-10-28 12:12:40 +0300

    Implement Dz3 first task

--- G58L3/ViewController.swift
+++ G58L3/ViewController.swift
@@ -12,14 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        someTaskFromDz3()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func someTaskFromDz3() {
+        let hello = 5
+        print(hello)
     }
-
 
 }
 
