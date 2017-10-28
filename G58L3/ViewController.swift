234253c16312da907a160b74f03f89cfaa95aa40
Tung Fam <tung@pixpie.co>
2017-10-28 12:00:51 +0300

    Initial commit

---
+++ G58L3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  G58L3
+//
+//  Created by Tung Fam on 10/28/17.
+//  Copyright © 2017 Tung Fam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
